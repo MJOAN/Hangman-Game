e3a67a18cdbed9efc179610e134c6823a589ebfa
Mariam Joan <royalton@gmail.com>
2017-09-02 23:46:53 -0700

    09-02-4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 
     // VARIABLES
       // ==========================================================================
-        var userGuses, hangmanWord, userGuess, lettersAlreadyGuessed;
+        var userGuess, hangmanWord, lettersAlreadyGuessed;
         var userWin, userLoss = 0;
         var placeHolderForWord = [];
         var guessesRemaining = 30;
@@ -16,32 +16,36 @@
    // MAIN PROCESS
       // ==============================================================================
 
+   // FUNCTIONS
+      // ==============================================================================
+      
       document.onkeyup = function(event) {
 
             var e = document.addEventListener("onkeyup", event);
             var userGuess = String.fromCharCode(event.keyCode).toLowerCase(e);
             var hangmanWord = chooseWord[Math.floor(Math.random() * chooseWord.length)];
+            var placeHolderForWord = [];
 
             for (var i = 0; i < hangmanWord.length; i++) {
                 placeHolderForWord[i] = "__";
             }
-            document.getElementById("hangman").innerHTML = placeHolderForWord[i] + "__";
-        }
-
-    // FUNCTIONS
-      // ==============================================================================
+            document.getElementById("hangman").innerHTML = placeHolderForWord.join(userGuess);  
+        }   
       
-        function hangmanGame() {  
-   
             if (userGuess !== placeHolderForWord || userGuess === !isNAN) {
+                var lettersAlreadyGuessed = placeHolderForWord.length;
+                for (var k = 0; k < placeHolderForWord.length; k++) {
+                    if (placeHolderForWord[i] !== "__") {
+                        lettersAlreadyGuessed --;
+                    }
+                document.getElementById("lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed.toUpperCase();
+            }  
                 lettersAlreadyGuessed++;
                 guessesRemaining--;
-            	
-            	document.getElementByID("hangman").innerHTML = userGuess;
+        
                 document.getElementById("message").innerHTML = "Please guess again! Enter any letter from A to Z!";
                 document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
                 document.getElementById("lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed;
-                console.log(guessesRemaining, lettersAlreadyGuessed);
 
             } else { 
             	for (var j = 0; j < hangmanWord.length; j++) {
@@ -56,17 +60,7 @@
                         document.getElementById("lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed;
                     } 
                 } 
-            } 
-
-            var lettersAlreadyGuessed = placeHolderForWord.length;
-            for (var k = 0; k < placeHolderForWord.length; k++) {
-                if (placeHolderForWord[i] !== "__") {
-                	lettersAlreadyGuessed;
-                }
-            document.getElementById("lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed.toUpperCase();
-            }  
-        }
-
+            }
 
     // WIN LOSS RESET FUNCTIONS
       // ==============================================================================
@@ -90,7 +84,4 @@
 
            	winLoss();
            	resetGame();
-
-
-
  
