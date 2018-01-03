35e45d7d0efe1aee893e71ab0f494a0436802ecd
Mariam Joan <royalton@gmail.com>
2018-01-02 17:44:32 -0800

    updated js and uploaded png

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,83 @@
 
+var guessLimit;
+var correctLetter;
+var wrongLetter;
+var word; 
+var wordArray;
+var userWin = 0;
+var userLoss = 0;
+var wordElement = document.getElementById('hangman');
+var letterCountElement = document.getElementById('letterCount');
+var lettersGuessedElement = document.getElementById('lettersGuessed');
 
-    // VARIABLES
-      // ==========================================================================
-        var userGuess, hangmanWord, lettersAlreadyGuessed;
-        var userWin, userLoss = 0;
-        var placeHolderForWord = [];
-        var guessesRemaining = 30;
+function startGame() {
+    wordArray = ["acoustics", "adiabatic", "albedo", "acceleration", "ampere",
+            "amplitued", "velocity", "electron", "capacitance", "centrifugal", "magnetism",
+            "momentum", "mole", "voltage", "nucleus", "joule", "achromatic", "antineutrino",
+            "Avogadro", "photon", "proton", "thermodynamics"
+            ];
 
-		var chooseWord = ["acoustics", "adiabatic", "albedo", "acceleration", "ampere",
-    		"amplitued", "velocity", "electron", "capacitance", "centrifugal", "magnetism",
-   			"momentum", "mole", "voltage", "nucleus", "joule", "achromatic", "antineutrino",
-    		"Avogadro", "photon", "proton", "thermodynamics"
-			];
+    word = wordArray[Math.floor(Math.random() * wordArray.length)];
+    guessLimit = word.length + 5;   // allowed guesses is guesses remaining lettercount div
+    wrongLetter = [];
+    correctLetter = [];
 
-   // MAIN PROCESS
-      // ==============================================================================
+  // initialize correctGuesses array with underscores
+  for (var i = 0; i < word.length; i++) {
+    correctLetter.push('_');
+  }
 
-   // FUNCTIONS
-      // ==============================================================================
-      
-      document.onkeyup = function(event) {
+  wordElement.innerHTML = correctLetter.join(' ');
+  letterCountElement.innerHTML = guessLimit;
+  console.log("guessLimit: ", guessLimit)
+}
 
-            var e = document.addEventListener("onkeyup", event);
-            var userGuess = String.fromCharCode(event.keyCode).toLowerCase(e);
-            var hangmanWord = chooseWord[Math.floor(Math.random() * chooseWord.length)];
-            var placeHolderForWord = [];
+function updateGuesses(letter) {
+  guessLimit--;
+  letterCountElement.innerHTML = guessLimit;
 
-            for (var i = 0; i < hangmanWord.length; i++) {
-                placeHolderForWord[i] = "__";
-            }
-            document.getElementById("hangman").innerHTML = placeHolderForWord.join(userGuess);  
-        }   
-      
-            if (userGuess !== placeHolderForWord || userGuess === !isNAN) {
-                var lettersAlreadyGuessed = placeHolderForWord.length;
-                for (var k = 0; k < placeHolderForWord.length; k++) {
-                    if (placeHolderForWord[i] !== "__") {
-                        lettersAlreadyGuessed --;
-                    }
-                document.getElementById("lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed.toUpperCase();
-            }  
-                lettersAlreadyGuessed++;
-                guessesRemaining--;
-        
-                document.getElementById("message").innerHTML = "Please guess again! Enter any letter from A to Z!";
-                document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-                document.getElementById("lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed;
+    if (word.indexOf(letter) === -1) { // letter is NOT in the word
+    wrongLetter.push(letter); 
+    lettersGuessedElement.innerHTML = "Letters Guessed: " + wrongLetter.join(' ');
+    
+    } else { 
+    
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
+        correctLetter[i] = letter;
+      }
+    }
+    wordElement.innerHTML = correctLetter.join(' ');
+  }
+}
 
-            } else { 
-            	for (var j = 0; j < hangmanWord.length; j++) {
-                    if (userGuess === hangmanWord[i]) {
-                        placeHolderforWord[i] = userGuess;
-                        lettersAlreadyGuessed++;
-                        guessesRemaining--;
+function winloss() {
+  if (correctLetter.indexOf('_') === -1) {
+        userWin++;
+        document.getElementById("message").innerHTML = "Congratulations! You've won the Hangman Challenge!";
+        document.getElementById("userWin").innerHTML = "Win: " + userWin;
+        letterCount.innerHTML = "";  
 
-						document.getElementByID("hangman").innerHTML = userGuess;
-                        document.getElementById("message").innerHTML = "Great work! You've got a letter!";
-                        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-                        document.getElementById("lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed;
-                    } 
-                } 
-            }
 
-    // WIN LOSS RESET FUNCTIONS
-      // ==============================================================================
+  } else if (guessLimit === 0) {
+        userLoss++;
+        document.getElementById("message").innerHTML = "You lost this game, please try again!";
+        document.getElementById("userLoss").innerHTML = "Loss: " + userLoss;
+        letterCount.innerHTML = "";  
+    }
+  }
 
-        function resetGame() {
-                document.getElementById("hangman").innerHTML = placeHolderForWord.join(" ");
-                }
-     
-        function winLoss() {
-            if (guessesRemaining === 0) {
-                userLoss++;
-                document.getElementById("message").innerHTML = "I'm sorry, you are all out of guesses! Please try again!";
-                document.getElementById("userLoss").innerHTML = userLoss;
+function resetGame() {
+    letterCountElement.innerHTML = "";
+    location.reload();
+}
 
-                } else if (lettersAlreadyGuessed === 0) { 
-                    userWin++;
-                    document.getElementById("message").innerHTML = "Congratulations! You've won the Hangman Challenge!";
-                    document.getElementById("userWin").innerHTML = userWin;
-                    }
-            } // end winLoss function
 
-           	winLoss();
-           	resetGame();
- 
+document.onkeyup = function (event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    // var regEx = /^[a-zA-Z]*$/;
+    updateGuesses(userGuess);
+    winloss();
+};
+
+startGame();
